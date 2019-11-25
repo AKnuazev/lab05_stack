ab7f2539908d1357acc6b4a1dfae69b01d1e4ba0
AKnuazev <2536040@gmail.com>
2019-11-25 13:24:22 +0300

    Added tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,43 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include "../include/Stack.h"
+//#include "../include/NoCopyStack.h"
+/*
+class Point
+{
+public:
+    int x;
+    int y;
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+    Point()
+    {
+        x = 0;
+        y = 0;
+    }
+    Point(int x1, int y1)
+    {
+        this->x = x1;
+        this->y = y1;
+    }
+    Point(const Point& pnt)
+    {
+        this->x = pnt.x;
+        this->y = pnt.y;
+    }
+};*/
+
+TEST(Stack_Test,AddElem_Test)
+{
+    Stack<int> stack;
+    stack.push(4);
+    stack.push(5);
 }
+/*
+TEST(NoCopyStack_Test, AddElem_Test)
+{
+    NoCopyStack<Point> stack;
+    stack.push(Point(3,4));
+    stack.push_emplace(5,6);
+}*/
+
